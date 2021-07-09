8b8557b7cea94d108319bb0e716fa8e486b260a1
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-07-09 11:45:40 -0500

    Week 3 Workshop Feedback Corrections

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const passport = require('passport');
 const authenticate = require('../authenticate');
 
 
-
 const router = express.Router();
 
 /* GET users listing. */
@@ -57,46 +56,6 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
-});
-
-router.post('/login', (req, res, next) => {
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const username = auth[0];
-    const password = auth[1];
-
-    User.findOne({ username: username })
-      .then(user => {
-        if (!user) {
-          const err = new Error(`User ${username} does not exist!`);
-          err.status = 401;
-          return next(err);
-        } else if (user.password !== password) {
-          const err = new Error('Your password is incorrect!');
-          err.status = 401;
-          return next(err);
-        } else if (user.username === username && user.password === password) {
-          req.session.user = 'authenticated';
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/plain');
-          res.end('You are authenticated!')
-        }
-      })
-      .catch(err => next(err));
-  } else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You are already authenticated!');
-  }
 });
 
 router.get('/logout', (req, res, next) => {
