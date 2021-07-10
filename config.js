d55d63d1d4dd437cdc291b3d8126b9d680bf9c2c
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-07-10 03:17:21 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '4376425855757253',
+        clientSecret: '1f41220c67444b3b411df1a18e3e6296'
+    }
 }
